9a12ba087018f8d10932fc2c1a2a14cef38aeeb5
djbb7 <daniel.bruzual@gmail.com>
2016-09-29 15:55:36 +0300

    make it better

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+
+var valid_user = {
+	"username" : "peterpan",
+	"password" : "dreamisover"
+};
+
+app.post('/login', function (req, res, next) {
+	if(!req.body.username || !req.body.password
+		|| req.body.username != valid_user.username
+		|| req.body.password != valid_user.password){
+		res.status(401).send();
+	} else {
+		return next();
+	}
+}, function (req, res){
+	//generate token
+	var token = '3434dd34434.12312esdsdsd'; 
+	res.send(token);
+});
+
+app.post('/logout', function (req, res){
+	res.send('Logout');
+});
+
+app.get('/application/list', function (req, res){});
+
+app.route('/application/:appid')
+
+.get(function(req, res){
+	res.send('App: '+req.params.appid);
+})
+
+.delete(function(req, res){
+
+});
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
+    });
