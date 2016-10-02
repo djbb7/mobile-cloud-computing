09fc1d8540e2597d0df24f549980a76e30981b89
Bruzual Balzan Daniel <bruzuad1@niksula.hut.fi>
2016-10-02 16:02:34 +0300

    fix typo

--- routes/applications.js
+++ routes/applications.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var router = express.Router();
-
+/*
 var valid_apps = [{
 	"name" : "Open Office Writer",
 	"id"	: "openoffice",
 	"icon_url" : "http://somethingsomething.com"
 }];
 
-router.get('/application/list', function (req, res){});
+var isInsideOtakari = function(lat, lng){
+	return true;
+};
+*/
+router.get('/hola', function (req, res){
+/*	var lat = req.query.lat;
+	var lng = req.query.lng;
+	var isInside = false;
+	if(lat && lng){
+		isInside = isInsideOtakari(lat, lng);
+	} 
+
+	//TODO: order app list
+
+	req.send(valid_apps);*/
+	res.send("hola");
+});
 
 
 router.route('/application/:appid')
