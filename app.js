b902894f7f22fa09350339dc43fb182222c90c5d
Bruzual Balzan Daniel <bruzuad1@niksula.hut.fi>
2016-10-02 12:57:27 +0300

    Split into modules

--- app.js
+++ app.js
@@ -1,45 +1,43 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
+
+var users = require('./routes/users');
+var appManager = require('./routes/applications');
+
 var app = express();
 
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json()); // for parsing application/json message body
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var valid_user = {
-	"username" : "peterpan",
-	"password" : "dreamisover"
-};
 
-app.post('/login', function (req, res, next) {
-	if(!req.body.username || !req.body.password
-		|| req.body.username != valid_user.username
-		|| req.body.password != valid_user.password){
-		res.status(401).send();
-	} else {
-		return next();
-	}
-}, function (req, res){
-	//generate token
-	var token = '3434dd34434.12312esdsdsd'; 
-	res.send(token);
+app.use('/users', users);
+app.use('/application', appManager);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.post('/logout', function (req, res){
-	res.send('Logout');
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
 });
 
-app.get('/application/list', function (req, res){});
 
-app.route('/application/:appid')
+module.exports = app;
 
-.get(function(req, res){
-	res.send('App: '+req.params.appid);
-})
 
-.delete(function(req, res){
-
-});
-
-var PORT = process.env.PORT || 5000;
-app.listen(PORT, function () {
-	console.log('Example app listening on port '+PORT);
-    });
