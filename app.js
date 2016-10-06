c941e542adf93e758de859ed6872ee407665fc63
Bruzual Balzan Daniel <bruzuad1@niksula.hut.fi>
2016-10-06 21:14:24 +0300

    Fix comments

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json()); // for parsing application/json message body
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/static', express.static('public'));
-app.use('/users', users);
-app.use('/application', applications);
+app.use('/static', express.static('public')); //logos of files
+app.use('/users', users); //session handling
+app.use('/application', applications); //application list
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
