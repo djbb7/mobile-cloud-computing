361b8b7d53325cf9b8a25afc3d6fed859b999df3
Bruzual Balzan Daniel <bruzuad1@niksula.hut.fi>
2016-10-06 20:52:47 +0300

    Add static images

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json()); // for parsing application/json message body
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
+app.use('/static', express.static('public'));
 app.use('/users', users);
 app.use('/application', applications);
 
